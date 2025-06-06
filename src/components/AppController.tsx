a278f975ab13f601a048ba87fc9dfca06c113a62
Crans <108697172+0xcrans@users.noreply.github.com>
2025-06-06 07:07:51 +0200

    Update AppController.tsx

--- src/components/AppController.tsx
+++ src/components/AppController.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from 'react';
-import { MainLayout, MenuItem } from './MainLayout';
+import React from 'react';
 import { Money } from './Money';
-import { Product } from './Product';
 
 export function AppController() {
-  const [activeView, setActiveView] = useState<MenuItem>('money');
-
-  const content = (() => {
-    switch (activeView) {
-      case 'money':
-        return <Money />;
-      case 'product':
-        return <Product />;
-      case 'buy':
-        // This case will never be reached since the click is handled in MainLayout
-        return <Money />;
-      case 'farm':
-        // This case will never be reached since the click is disabled in MainLayout
-        return <Money />;
-      default:
-        return <Money />;
-    }
-  })();
-
   return (
-    <MainLayout 
-      onMenuItemClick={setActiveView} 
-      activeMenuItem={activeView}
-    >
-      {content}
-    </MainLayout>
+    <div style={{ 
+      width: '100%',
+      minHeight: '100vh',
+      backgroundColor: '#181825',
+      color: '#cdd6f4',
+      padding: '20px',
+      boxSizing: 'border-box'
+    }}>
+      <Money />
+    </div>
   );
 } 
